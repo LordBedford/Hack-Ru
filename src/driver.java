e665bfd0518d87f6dab3bb1e6e5cf9875623acb6
Andrew Cater <Andrew Cater>
2016-04-16 15:04:56 -0400

    Driver

--- src/driver.java
+++ src/driver.java
@@ -1,4 +1,47 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class driver {
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class driver extends JPanel
+{
+	public static void main(String[] args) 
+	{
+		JFrame frame = new JFrame ("Game thing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		driver panel = new driver();
+		frame.getContentPane().add(panel);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask(){public void run(){panel.tick();panel.repaint();
+		}}, 0, 1000/60);//handles tick and repainting the jframe.
+		
+	}
+	public driver ()
+	{
+		this.setPreferredSize(new Dimension(1000,1000));
+	}
+	public void tick ()
+	{
+		
+	}
+	public void paintComponent (Graphics g)
+	{
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File("res/blood.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		g.drawImage(image, 0, 0, null);
+	}
 }
