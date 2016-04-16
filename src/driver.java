3198e0a736180090b15a517b2ae9b2fb3a074fe8
Andrew Cater <Andrew Cater>
2016-04-16 19:01:10 -0400

    Monster Spawns

--- src/driver.java
+++ src/driver.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -20,6 +21,9 @@ public class driver extends JPanel implements KeyListener, MouseMotionListener
 	private int mouseX, mouseY;
 	public static Player player;
 	private Monster monster;
+	private ArrayList<Entity> creatures = new ArrayList();
+	private int monsterSpawnRate = 500;//Spawns monsters every x ticks
+	private int monsterSpawnCounter = 0;//Counts ticks till monster spawn
 	public static void main(String[] args) 
 	{
 		JFrame frame = new JFrame ("Game thing");
@@ -33,6 +37,7 @@ public class driver extends JPanel implements KeyListener, MouseMotionListener
 		Timer timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask(){public void run(){panel.tick();panel.repaint();
 		}}, 0, 1000/60);//handles tick and repainting the jframe.
+		 
 		
 	}
 	public driver ()
@@ -42,14 +47,23 @@ public class driver extends JPanel implements KeyListener, MouseMotionListener
 		requestFocus();
 		addKeyListener(this);
 		addMouseMotionListener(this);
-		player = new Player(100,4,0,0);
-		monster = new Monster (100,2,0,0,0);
+		creatures.add(player = new Player(100,4,0,0));
+		creatures.add(monster = new Monster (100,2,0,0,0));
 	}
 	//update
 	public void tick ()
 	{
-		player.update();
-		monster.update();
+		if(monsterSpawnCounter == monsterSpawnRate)
+		{
+			creatures.add(new Monster(100,2,0,0,0));
+			monsterSpawnCounter = 0;
+		}
+		else
+			monsterSpawnCounter++;
+		for(int i = 0; i < creatures.size();i++)//updates all entities in the array.
+		{
+			creatures.get(i).update();
+		}
 	}
 	
 	//render
